5ae196a8f2fdfe9c81f5f00c15d23ff0c17225ce
karldreta <karldreta@gmail.com>
2024-07-24 01:26:01 +0800

    Add cart UX

--- src/viewCart.js
+++ src/viewCart.js
@@ -1,28 +1,39 @@
 const cartBtn = document.querySelector('#viewCart');
 const imageContainer = cartBtn.closest('.imageContainer');
 
-cartBtn.addEventListener('click', viewCart)
-    
-export default function viewCart () {
+cartBtn.addEventListener('click', viewCart);
+
+export default function viewCart() {
     let tooltip = imageContainer.querySelector('.tooltip');
     
     if (!tooltip) {
-        // Create tooltip if it doesn't exist
         tooltip = document.createElement('div');
         tooltip.classList.add('tooltip');
         tooltip.innerHTML = `
             <h2>Cart</h2>
             <section>
-            <ul>
-            <li>
-            </li>
-            </ul>
+                <ul id="productList"></ul>
             </section>
-            <button id='checkout'>Checkout</button>
+            <button id="checkout">Checkout</button>
         `;
         imageContainer.appendChild(tooltip);
     }
     
-    // Toggle the 'show' class to show/hide the tooltip
     tooltip.classList.toggle('show');
-};
+}
+
+export function addProductToTooltip(productHTML) {
+    let tooltip = imageContainer.querySelector('.tooltip');
+    
+    if (!tooltip) {
+        // Create the tooltip if it doesn't exist
+        viewCart();
+        tooltip = imageContainer.querySelector('.tooltip');
+    }
+    
+    const ul = tooltip.querySelector('#productList');
+    const productListItem = document.createElement('li');
+    productListItem.classList.add('productListItem');
+    productListItem.innerHTML = productHTML;
+    ul.appendChild(productListItem);
+}
