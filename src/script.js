59a4a254aa625fdb93eae8cd6f1b90757e354765
karldreta <karldreta@gmail.com>
2024-07-23 09:01:18 +0800

    Add font

--- src/script.js
+++ src/script.js
@@ -3,15 +3,15 @@ import './style.css';
 
 
 
-function createFooter() {
-    const footer = document.createElement('footer');
-    footer.classList.add('attribution');
-    footer.innerHTML = `
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="https://github.com/karldreta" target="_blank">karlDreta</a>.
-    `;
+// function createFooter() {
+//     const footer = document.createElement('footer');
+//     footer.classList.add('attribution');
+//     footer.innerHTML = `
+//         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+//     Coded by <a href="https://github.com/karldreta" target="_blank">karlDreta</a>.
+//     `;
 
-    return footer;
-}
+//     return footer;
+// }
 
-document.body.appendChild(createFooter())
+// document.body.appendChild(createFooter())
