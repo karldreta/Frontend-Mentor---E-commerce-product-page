bc9ba3be71af23f32500743e2c444359c94deada
karldreta <karldreta@gmail.com>
2024-08-28 02:20:41 +0800

    Fix initial clicked img

--- src/lightbox.js
+++ src/lightbox.js
@@ -7,26 +7,30 @@ productThumbnail.forEach((thumbnail) =>
   thumbnail.addEventListener("click", expandThumbnail),
 );
 
-
 let slideIndex = 1;
 
 export default function expandThumbnail(e) {
   const dialog = document.querySelector("#dialog");
+
+  if (e.target.dataset.pos <= 4 && e.target.dataset.pos !== 0) {
+    slideIndex = e.target.dataset.pos;
+  } else {
+    slideIndex = 1;
+  }
   dialog.showModal();
-  slideIndex = e.target.dataset.pos;
   pictureSlide();
-  
-  dialog.addEventListener("click", (e) => {
-    const dialogDimensions = dialog.getBoundingClientRect();
-    if (
-      e.clientX < dialogDimensions.left ||
-      e.clientX > dialogDimensions.right ||
-      e.clientY < dialogDimensions.top ||
-      e.clientY > dialogDimensions.bottom
-    ) {
-      dialog.close();
-    }
-  });
+
+  // dialog.addEventListener("click", (e) => {
+  //   const dialogDimensions = dialog.getBoundingClientRect();
+  //   if (
+  //     e.clientX < dialogDimensions.left ||
+  //     e.clientX > dialogDimensions.right ||
+  //     e.clientY < dialogDimensions.top ||
+  //     e.clientY > dialogDimensions.bottom
+  //   ) {
+  //     dialog.close();
+  //   }
+  // });
 }
 
 const closeBtn = document.querySelector(".closeModal");
